f2eba4b9e8b7f3231deb58355687fb9c20881366
omoefe <raniem57@gmail.com>
2025-11-16 16:56:37 +0100

    fixed the NEXT API

--- src/app/api/withdrawal/notify-user/route.js
+++ src/app/api/withdrawal/notify-user/route.js
@@ -1,6 +1,7 @@
 // app/api/withdrawal/notify-user/route.js
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
+//
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const ADMIN_EMAIL =
